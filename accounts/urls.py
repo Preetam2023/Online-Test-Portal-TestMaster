a9ab3ceb8daaa880de5989622686156c415f5bd5
Preetam Manna <preetammanna.cse@gmail.com>
2024-10-12 17:58:19 +0530

    profile page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home, login_view, signup,dashboard
+from .views import home, login_view, signup,dashboard,profile, practice_questions,mock_test
 urlpatterns = [
+    path('', home, name='home'),
     path('signup/', signup, name='signup'),
-    path('', home, name='home'), 
     path('login/', login_view, name='login'),
     path('dashboard/', dashboard, name='dashboard'),
+    path('profile/', profile, name='profile'),
+    path('practice-questions/', practice_questions, name='practice_questions'), 
+    path('mock-test/', mock_test, name='mock_test'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.contrib import messages
 from .forms import LoginForm, SignupForm
 from django.contrib.auth.decorators import login_required
+from .models import CustomUser
 
 User = get_user_model()  # Use the custom user model
 
@@ -59,3 +60,22 @@ def login_view(request):
 @login_required
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')  # Create a template for this
+
+@login_required
+def profile(request):
+    # Assuming that test history will be updated in the future
+    
+    test_history = []  # Placeholder for future test history data
+    return render(request, 'accounts/profile.html', {
+        'user': request.user,
+        'test_history': test_history
+    })
+    
+
+@login_required    
+def practice_questions(request):
+    return render(request, 'accounts/practice_questions.html')
+
+@login_required    
+def mock_test(request):
+    return render(request, 'accounts/mock_test.html')
